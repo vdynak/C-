1aa6c8a7e78360d869b060b1d035fa710a89be37
Victoria <116195324+vdynak@users.noreply.github.com>
2023-07-14 00:47:01 -0400

    Add files via upload

---
+++ Lab1/gas.cpp
@@ -0,0 +1,30 @@
+/*
+ Victoria Dynak
+ Course: CSCI 136
+ Instructor: Genady Maryash
+ Assignment: Homework E2.10
+ 
+ Prompts the user for the gallons, effiency, and price per gallon to then calculate and return the cost per 100 miles and the distance that could be traveled with the current amount of gas.
+ */
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//    float gallons;
+//    float efficiency;
+//    float price;
+//    float cost;
+//    float distance;
+//    cout<<"Enter the number of gallons of gas in the tank: "; //20
+//    cin>>gallons;
+//    cout<<"Enter the fuel efficiency in miles per gallon: "; //10
+//    cin>>efficiency;
+//    cout<<"Enter the price of gas per gallon: "; //5
+//    cin>>price;
+//    cost = (price * 100 /efficiency);
+//    distance = gallons * efficiency;
+//    cout<<"Cost per 100 miles: "<<cost<<endl;
+//    cout<<"Travel with current gas: "<<distance<<" miles"<<endl;
+//    return 0;
+//}
+//
---
+++ Lab1/leap.cpp
@@ -0,0 +1,22 @@
+/*
+ Victoria Dynak
+ Course: CSCI-136
+ Instructor: Genady Maryash
+ Assignment: leap.cpp
+ Prompts the user to enter a year number. The program then returns whether the following year is a common year or leap year based on the calculations that qualify a year as one or the other.
+ */
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//    int year;
+//    cout<<"Enter year: ";
+//    cin>>year;
+//    if(year%4==0 and (!(year%100==0)or(year%400==0))){
+//        cout<<"Leap year"<<endl;
+//    }
+//    else{
+//        cout<<"Common year"<<endl;
+//    }
+//    return 0;
+//}
