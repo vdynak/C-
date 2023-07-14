8521622e191282c54f07d9e88a80f7d9d55aa945
Victoria <116195324+vdynak@users.noreply.github.com>
2023-07-14 00:48:46 -0400

    Update smaller.cpp

--- Lab1/smaller.cpp
+++ Lab1/smaller.cpp
@@ -1,24 +1,18 @@
-/*
- Victoria Dynak
- Course: CSCI-136
- Instructor: Genady Maryash
- Assignment: smaller.cpp
- Find the smaller of two numbers and return that number.
- */
+//Find the smaller of two numbers and return that number.
 
-//#include <iostream>
-//using namespace std;
-//int main(){
-//    int one, two;
-//    cout<<"Enter the first number: ";
-//    cin>>one;
-//    cout<<"Enter the second number: ";
-//    cin>>two;
-//    if(one>two){
-//        cout<<"The smaller of the two is "<<two<<endl;
-//    }
-//    else{
-//        cout<<"The smaller of the two is "<<one<<endl;
-//    }
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+int main(){
+   int one, two;
+   cout<<"Enter the first number: ";
+   cin>>one;
+   cout<<"Enter the second number: ";
+   cin>>two;
+   if(one>two){
+       cout<<"The smaller of the two is "<<two<<endl;
+   }
+   else{
+       cout<<"The smaller of the two is "<<one<<endl;
+   }
+   return 0;
+}
