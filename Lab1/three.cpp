b2b520b6609566b943630ba5108b13eb20a91659
Victoria <116195324+vdynak@users.noreply.github.com>
2023-07-14 00:50:03 -0400

    Update three.cpp

--- Lab1/three.cpp
+++ Lab1/three.cpp
@@ -1,25 +1,17 @@
-/*
- Victoria Dynak
- Course: CSCI 136
- Instructor: Genady Maryash
- Assignment: Homework E1.7
- 
- The program prompts the user to enter three items and then returns each one of them on a seperate line.
- */
+//The program prompts the user to enter three items and then returns each one of them on a separate line.
 
-//#include <iostream>
-//using namespace std;
-//int main(){
-//    string x;
-//    string y;
-//    string z;
-//    cout<<"Enter an item: ";
-//    cin>>x;
-//    cout<<"Enter a second item: ";
-//    cin>>y;
-//    cout<<"Enter a third item: ";
-//    cin>>z;
-//    cout<<x<<endl<<y<<endl<<z<<endl;
-//    return 0;
-//}
-
+#include <iostream>
+using namespace std;
+int main(){
+   string x;
+   string y;
+   string z;
+   cout<<"Enter an item: ";
+   cin>>x;
+   cout<<"Enter a second item: ";
+   cin>>y;
+   cout<<"Enter a third item: ";
+   cin>>z;
+   cout<<x<<endl<<y<<endl<<z<<endl;
+   return 0;
+}
