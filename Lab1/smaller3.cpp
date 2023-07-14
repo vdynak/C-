75f60f55c9d8f6fde6a73214641704d665428598
Victoria <116195324+vdynak@users.noreply.github.com>
2023-07-14 00:49:14 -0400

    Update smaller3.cpp

--- Lab1/smaller3.cpp
+++ Lab1/smaller3.cpp
@@ -1,33 +1,27 @@
-/*
- Victoria Dynak
- Course: CSCI-136
- Instructor: Genady Maryash
- Assignment: smaller3.cpp
- Prompts the user for three numbers and returns the smallest of the three values.
- */
+//Prompts the user for three numbers and returns the smallest of the three values.
 
-//#include <iostream>
-//using namespace std;
-//int main(){
-//    int x,y,z;
-//    cout<<"Enter the first number: ";
-//    cin>>x;
-//    cout<<"Enter the second number: ";
-//    cin>>y;
-//    cout<<"Enter the third number: ";
-//    cin>>z;
-//    int min;
-//    if(x>y){
-//        min=y;
-//    }
-//    else{
-//        min=x;
-//    }
-//    if(min>z){
-//        cout<<"The smaller of the three is "<<z<<endl;
-//    }
-//    else{
-//        cout<<"The smaller of the three is "<<min<<endl;
-//    }
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+int main(){
+   int x,y,z;
+   cout<<"Enter the first number: ";
+   cin>>x;
+   cout<<"Enter the second number: ";
+   cin>>y;
+   cout<<"Enter the third number: ";
+   cin>>z;
+   int min;
+   if(x>y){
+       min=y;
+   }
+   else{
+       min=x;
+   }
+   if(min>z){
+       cout<<"The smaller of the three is "<<z<<endl;
+   }
+   else{
+       cout<<"The smaller of the three is "<<min<<endl;
+   }
+   return 0;
+}
