93278f34aa67e7dbdaef3d81e185ee2f27d9c4a2
Victoria <116195324+vdynak@users.noreply.github.com>
2023-07-14 01:24:47 -0400

    Create poem.cpp

---
+++ Lab9/poem.cpp
@@ -0,0 +1,20 @@
+//Add a delete operator to release the memory.
+
+#include <iostream>
+using namespace std;
+
+string * createAPoemDynamically() {
+    string *p = new string;
+    *p = "Roses are red, violets are blue";
+    return p;
+}
+
+int main() {
+    while(true) {
+        string *p;
+        p = createAPoemDynamically();
+
+        // assume that the poem p is not needed at this point
+        delete p;
+    }
+}
