b213b90c65f535eb19c614cc579b6ba6296715e0
Victoria <116195324+vdynak@users.noreply.github.com>
2023-07-14 00:51:58 -0400

    Add files via upload

---
+++ Lab2/print-interval.cpp
@@ -0,0 +1,41 @@
+/*
+ Victoria Dynak
+ Course: CSCI 136
+ Instructor: Genady Maryash
+ Assignment: print-interval.cpp
+ Ask the user for a lower and upper bound, respectively assigned to the L and U variables. The program prints all the numbers between L and U, L inclusive, U exclusive.
+ */
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main(){
+//    int L;
+//    int U;
+//    const string newspace = " ";
+//    cout<<"Please enter L: ";
+//    cin>>L;
+//    cout<<"Please enter U: ";
+//    cin>>U;
+//    for (int i = L; i < U; i++){
+//        cout<<i<<newspace;
+//    }
+//    cout<<endl;
+//    return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//    int lower;
+//    int upper;
+//    cout<<"Enter a lower limti: ";
+//    cin>>lower;
+//    cout<<"Enter an upper limit: ";
+//    cin>>upper;
+//    for(int i = lower; i < upper; i++){
+//        cout<<i<<" ";
+//    }
+//    cout<<endl;
+//    return 0;
+//}
